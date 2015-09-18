0cbf1c9ec64d52d60a670d47f9e14df06f7995d5
mollahosseini <ali.mollahosseini@gmail.com>
2015-09-18 09:27:03 -0600

    Update sort.cpp
    A new change directly from gitHub website

--- sort.cpp
+++ sort.cpp
@@ -2,11 +2,13 @@
 
 // this is a new comment on this file
 //using namespace std;
+// This file is edited from GitHub
 
 int main(){
 
 	int *p=new int[15];
 	delete[] p;
-
+	for (int i=0; i<10; ++i)
+	  cout << i ;	
 	return 0;
 }
